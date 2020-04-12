c3fe9eac3d2c377940b54a37f4556be1a12ba3db
ttuom1nen <ttuominen@outlook.com>
2020-04-12 12:33:38 +0300

    Added interface for body

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from "express";
 
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get("/login", (req: Request, res: Response) => {
@@ -18,11 +22,14 @@ router.get("/login", (req: Request, res: Response) => {
   `);
 });
 
-router.post("/login", (req: Request, res: Response) => {
+router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  // Testing
-  res.send(email + password);
+  if (email && password) {
+    res.send(email + password);
+  } else {
+    res.send("Please fill in the information!");
+  }
 });
 
 export { router };
