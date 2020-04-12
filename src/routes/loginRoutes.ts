7395c32aefab31be6889d1670d3f22bf452192fd
ttuom1nen <ttuominen@outlook.com>
2020-04-12 13:35:43 +0300

    Input checks and logout

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -25,11 +25,35 @@ router.get("/login", (req: Request, res: Response) => {
 router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  if (email && password) {
-    res.send(email + password);
+  if (email && password && email === "xx" && password === "pw") {
+    req.session = { loggedIn: true };
+    res.redirect("/");
   } else {
-    res.send("Please fill in the information!");
+    res.send("Invalid email or password");
   }
+});
+
+router.get("/", (req: RequestWithBody, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(
+      `<div>
+        <div>You are logged in</div>
+        <a href="/logout">Log out</a>
+      </div>`
+    );
+  } else {
+    res.send(
+      `<div>
+      <div>You are not logged in</div>
+      <a href="/login">Login</a>
+    </div>`
+    );
+  }
+});
+
+router.get("/logout", (req: RequestWithBody, res: Response) => {
+  req.session = undefined;
+  res.redirect("/");
 });
 
 export { router };
