6b2c629ee590c5bca23b6920f67f6aa14ccbe500
ttuom1nen <ttuominen@outlook.com>
2020-04-16 22:20:33 +0300

    Capitalized enum keys

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,17 +1,18 @@
 import "reflect-metadata";
 import { Methods } from "./Methods";
+import { MetadataKeys } from "./MetadataKeys";
 
 function routeBinder(method: string) {
   return function get(path: string) {
     return function (target: any, key: string, desc: PropertyDescriptor) {
-      Reflect.defineMetadata("path", path, target, key);
-      Reflect.defineMetadata("method", "get", target, key);
+      Reflect.defineMetadata(MetadataKeys.Path, path, target, key);
+      Reflect.defineMetadata(MetadataKeys.Method, "get", target, key);
     };
   };
 }
 
-export const get = routeBinder(Methods.get);
-export const put = routeBinder(Methods.put);
-export const post = routeBinder(Methods.post);
-export const del = routeBinder(Methods.del);
-export const patch = routeBinder(Methods.patch);
+export const get = routeBinder(Methods.Get);
+export const put = routeBinder(Methods.Put);
+export const post = routeBinder(Methods.Post);
+export const del = routeBinder(Methods.Del);
+export const patch = routeBinder(Methods.Patch);
