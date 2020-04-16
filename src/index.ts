81c5095c07623be7dd8ca9ecd70a5558dc9e1595
ttuom1nen <ttuominen@outlook.com>
2020-04-16 20:18:36 +0300

    Implemented decorators

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import { router } from "./routes/loginRoutes";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
 
+import { router as controllerRouter } from "./controllers/decorators/controller";
+import "./controllers/LoginController";
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["randomstring"] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
